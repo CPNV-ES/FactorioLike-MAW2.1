15e27dfebde4d86e992b4b700f81d3e4f2540c49
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-21 11:35:42 +0100

    Create SpriteRenderer.cs

---
+++ MechanoCraft/SpriteRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MechanoCraft
+{
+    public class SpriteRenderer
+    {
+        Texture2D sprite;
+        Color color;
+        bool flip;
+
+        public SpriteRenderer(Texture2D sprite, Color color, bool flip = false )
+        {
+            Sprite = sprite;
+            Color = color;
+            Flip = flip;
+
+        }
+
+        public Texture2D Sprite 
+        {
+            get { return sprite; } 
+            set { sprite = value; } 
+        }
+        public Color Color 
+        { 
+            get { return color; }
+            set {  color = value; }
+        }
+        public bool Flip 
+        { 
+            get { return flip; } 
+            set {  flip = value; } 
+        }
+    }
+}
