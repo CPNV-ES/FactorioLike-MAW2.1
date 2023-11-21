df8c080ee248e24dfb89a7c099f8a0f89baf5542
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-21 11:35:43 +0100

    Create RenderSystem.cs

---
+++ MechanoCraft/RenderSystem.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.Entities;
+using MonoGame.Extended.Entities.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanoCraft
+{
+    public class RenderSystem : EntityDrawSystem
+    {
+        private readonly GraphicsDevice _graphicsDevice;
+        private readonly SpriteBatch _spriteBatch;
+
+        private ComponentMapper<Transform2> _transformMapper;
+        private ComponentMapper<SpriteRenderer> _spriteRendererMapper;
+
+        public RenderSystem(GraphicsDevice graphicsDevice)
+            : base(Aspect.All(typeof(Transform2), typeof(SpriteRenderer)))
+        {
+            _graphicsDevice = graphicsDevice;
+            _spriteBatch = new SpriteBatch(graphicsDevice);
+        }
+
+        public override void Initialize(IComponentMapperService mapperService)
+        {
+            _transformMapper = mapperService.GetMapper<Transform2>();
+            _spriteRendererMapper = mapperService.GetMapper<SpriteRenderer>();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _graphicsDevice.Clear(Color.Wheat);
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+            foreach (var entity in ActiveEntities)
+            {
+                var transform = _transformMapper.Get(entity);
+                var spriteRenderer = _spriteRendererMapper.Get(entity);
+
+                _spriteBatch.Draw(spriteRenderer.Sprite, transform.Position, null, spriteRenderer.Color);
+            }
+            _spriteBatch.End();
+        }
+    }
+}
