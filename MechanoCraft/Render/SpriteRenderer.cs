dd5fed4a5ec0b4ec3ca4d34d456d962d5009f6bf
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-23 12:03:20 +0100

    feat: added draw mode check
    when placing, it's good to place on center of the entity

--- MechanoCraft/Render/SpriteRenderer.cs
+++ MechanoCraft/Render/SpriteRenderer.cs
@@ -3,20 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.Entities;
 using MonoGame.Extended.Entities.Systems;
-using MonoGame.Extended.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MechanoCraft.Render
+namespace MechanoCraft
 {
-    public class SpriteRenderSystem : EntityDrawSystem
+    public class RenderSystem : EntityDrawSystem
     {
         private readonly GraphicsDevice _graphicsDevice;
         private readonly SpriteBatch _spriteBatch;
 
         private ComponentMapper<Transform2> _transformMapper;
-        private ComponentMapper<Sprite> _spriteMapper;
+        private ComponentMapper<SpriteRenderer> _spriteRendererMapper;
 
-        public SpriteRenderSystem(GraphicsDevice graphicsDevice)
-            : base(Aspect.All(typeof(Transform2), typeof(Sprite)))
+        public RenderSystem(GraphicsDevice graphicsDevice)
+            : base(Aspect.All(typeof(Transform2), typeof(SpriteRenderer)))
         {
             _graphicsDevice = graphicsDevice;
             _spriteBatch = new SpriteBatch(graphicsDevice);
@@ -25,7 +29,7 @@ namespace MechanoCraft.Render
         public override void Initialize(IComponentMapperService mapperService)
         {
             _transformMapper = mapperService.GetMapper<Transform2>();
-            _spriteMapper = mapperService.GetMapper<Sprite>();
+            _spriteRendererMapper = mapperService.GetMapper<SpriteRenderer>();
         }
 
         public override void Draw(GameTime gameTime)
@@ -36,9 +40,19 @@ namespace MechanoCraft.Render
             foreach (var entity in ActiveEntities)
             {
                 var transform = _transformMapper.Get(entity);
-                var sprite = _spriteMapper.Get(entity);
+                var spriteRenderer = _spriteRendererMapper.Get(entity);
 
-                _spriteBatch.Draw(sprite, transform);
+                Vector2 origin;
+                if (spriteRenderer.drawMode == SpriteRenderer.DrawMode.Center)
+                {
+                    origin = new Vector2(spriteRenderer.Sprite.Width/2,spriteRenderer.Sprite.Height / 2);
+                }
+                else
+                {
+                    origin = Vector2.Zero;
+                }
+
+                _spriteBatch.Draw(spriteRenderer.Sprite, transform.Position, null, spriteRenderer.Color, transform.Rotation, origin, transform.Scale, SpriteEffects.None, spriteRenderer.Layer);
             }
             _spriteBatch.End();
         }
