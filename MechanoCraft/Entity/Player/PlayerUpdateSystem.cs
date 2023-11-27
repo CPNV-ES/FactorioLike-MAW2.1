754c1d4defa36160c0f3993a6a0f948878bb2131
Vivien Piccin <Vivien.Piccin@eduvaud.ch>
2023-11-27 15:51:05 +0100

    Add basic player entity

--- MechanoCraft/Entity/Player/PlayerUpdateSystem.cs
+++ MechanoCraft/Entity/Player/PlayerUpdateSystem.cs
@@ -1,28 +1,65 @@
-﻿using Microsoft.Xna.Framework;
+﻿using MechanoCraft.Input;
+using Microsoft.Xna.Framework;
 using MonoGame.Extended;
 using MonoGame.Extended.Entities;
 using MonoGame.Extended.Entities.Systems;
-using System;
+using MonoGame.Extended.Sprites;
 
 namespace MechanoCraft.Entity.Player
 {
     public class PlayerUpdateSystem : EntityUpdateSystem
     {
         private ComponentMapper<Transform2> _transformMapper;
+        private ComponentMapper<Sprite> _spriteMapper;
         private ComponentMapper<Player> _playerMapper;
-        public PlayerUpdateSystem() : base(Aspect.All(typeof(Transform2), typeof(Player)))
+
+        private static readonly Vector2 UNIT_X_VECTOR = new Vector2(1f, 0f);
+        private static readonly Vector2 UNIT_Y_VECTOR  = new Vector2(0f, 1f);
+        private static readonly float PLAYER_BASE_SPEED = 20f;
+        public PlayerUpdateSystem() : base(Aspect.All(typeof(Transform2), typeof(Sprite), typeof(Player)))
         {
         }
 
         public override void Initialize(IComponentMapperService mapperService)
         {
             _transformMapper = mapperService.GetMapper<Transform2>();
+            _spriteMapper = mapperService.GetMapper<Sprite>();
             _playerMapper = mapperService.GetMapper<Player>();
+
+            InputHandler.GetInstance().AddInputListener(Microsoft.Xna.Framework.Input.Keys.W, () =>
+            {
+                foreach (int entity in ActiveEntities)
+                {
+                    _transformMapper.Get(entity).Position += UNIT_Y_VECTOR * -PLAYER_BASE_SPEED;
+                }
+            });
+
+            InputHandler.GetInstance().AddInputListener(Microsoft.Xna.Framework.Input.Keys.S, () =>
+            {
+                foreach (int entity in ActiveEntities)
+                {
+                    _transformMapper.Get(entity).Position += UNIT_Y_VECTOR * PLAYER_BASE_SPEED;
+                }
+            });
+            InputHandler.GetInstance().AddInputListener(Microsoft.Xna.Framework.Input.Keys.A, () =>
+            {
+                foreach (int entity in ActiveEntities)
+                {
+                    _transformMapper.Get(entity).Position += UNIT_X_VECTOR * -PLAYER_BASE_SPEED;
+                }
+            });
+            InputHandler.GetInstance().AddInputListener(Microsoft.Xna.Framework.Input.Keys.D, () =>
+            {
+                foreach (int entity in ActiveEntities)
+                {
+                    _transformMapper.Get(entity).Position += UNIT_X_VECTOR * PLAYER_BASE_SPEED;
+                }
+            });
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
