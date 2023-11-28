47de5451766035748f3d8758f3cbe2d666fce763
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-28 10:44:27 +0100

    feat: added placement system

--- MechanoCraft/MechanoCraft.cs
+++ MechanoCraft/MechanoCraft.cs
@@ -8,6 +8,7 @@ using MechanoCraft.Input;
 using MechanoCraft.Render;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.TextureAtlases;
+using MechanoCraft.Placement;
 
 namespace MechanoCraft
 {
@@ -30,13 +31,9 @@ namespace MechanoCraft
             // TODO: Add your initialization logic here
             _world = new WorldBuilder().AddSystem(new SpriteRenderSystem(GraphicsDevice)).Build();
             Components.Add(_world);
-            Entity entity = _world.CreateEntity();
             InputHandler.GetInstance().AddInputListener(Keys.Space, () =>
             {
-                Entity entity1 = _world.CreateEntity();
-                entity1.Attach( new Transform2());
-                entity1.Attach( new Sprite(Content.Load<Texture2D>("Crafter")));
-                entity1.Get<Transform2>().Position = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+
             });
             InputHandler.GetInstance().AddInputListener(Keys.Escape, () =>
             {
---
+++ MechanoCraft/Placement/ObjectPlacerSystem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using MonoGame.Extended.Entities;
+using MonoGame.Extended;
+using MechanoCraft.Input;
+using MechanoCraft.Render;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.TextureAtlases;
+using Microsoft.Xna.Framework.Content;
+
+namespace MechanoCraft.Placement
+{
+    public class ObjectPlacerSystem
+    {
+        public static void Place(Entity entity, Vector2 position)
+        {
+            entity.Get<Transform2>().Position = position;
+        }
+    }
+}
