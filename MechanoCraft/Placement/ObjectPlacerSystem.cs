f8d314dc8fed7279acd15b66b078be28e7660e40
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-11-28 11:05:15 +0100

    Update ObjectPlacerSystem.cs

--- MechanoCraft/Placement/ObjectPlacerSystem.cs
+++ MechanoCraft/Placement/ObjectPlacerSystem.cs
@@ -1,18 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Collections.Generic;
 using MonoGame.Extended.Entities;
 using MonoGame.Extended;
-using MechanoCraft.Input;
-using MechanoCraft.Render;
-using MonoGame.Extended.Sprites;
-using MonoGame.Extended.TextureAtlases;
-using Microsoft.Xna.Framework.Content;
+
 
 namespace MechanoCraft.Placement
 {
-    public class ObjectPlacerSystem
+    public static class ObjectPlacerSystem
     {
         public static void Place(Entity entity, Vector2 position)
         {
