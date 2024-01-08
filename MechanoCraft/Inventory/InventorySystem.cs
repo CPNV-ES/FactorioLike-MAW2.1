14dc731fba9997556cc0f4d39b6959ea24c13f86
Vivien Piccin <Vivien.Piccin@eduvaud.ch>
2024-01-08 13:59:59 +0100

    Add inventory system

---
+++ MechanoCraft/Inventory/InventorySystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MechanoCraft.Inventory.Items;
+using MonoGame.Extended.Entities;
+
+namespace MechanoCraft.Inventory
+{
+    public class InventorySystem
+    {
+        private static InventorySystem instance;
+        private Dictionary<IItem, Entity> worldResources;
+        private InventorySystem()
+        {
+            worldResources = new Dictionary<IItem, Entity>();
+        }
+
+        public void RegisterItem(IItem item, Entity owner)
+        {
+            worldResources.Add(item, owner);
+        }
+
+        public void TransferItem(IItem item, Entity newOwner)
+        {
+            worldResources[item] = newOwner;
+        }
+
+        public void RemoveItem(IItem item)
+        {
+            worldResources.Remove(item);
+        }
+
+        public List<IItem> GetItems(Entity owner) 
+        {
+            return worldResources.Keys.Where(item => worldResources[item] == owner).ToList();
+        }
+
+        public static InventorySystem GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new InventorySystem();
+            }
+            return instance;
+        }
+    }
+}
---
+++ MechanoCraft/Inventory/Items/CopperOre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanoCraft.Inventory.Items
+{
+    public class CopperOre : IItem
+    {
+        public string GetLocalizedName()
+        {
+            return "Copper Ore";
+        }
+    }
+}
