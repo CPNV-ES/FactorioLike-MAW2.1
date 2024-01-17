b2dd6934225e8b94311cba78cfa71ce201a10f7a
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-01-17 11:53:14 +0100

    feat: simplified instantiation

--- MechanoCraft/Inventory/ItemCreator.cs
+++ MechanoCraft/Inventory/ItemCreator.cs
@@ -12,12 +12,16 @@ namespace MechanoCraft.Inventory
 
         public static void CreateItems()
         {
-            possibleItems = new List<Item>();
-            possibleItems.Add(new Item(0, "IronOre", Item.ItemType.Ore, Item.MaterialType.Iron, Item.StateType.Solid));
-            possibleItems.Add(new Item(1, "Crafter", Item.ItemType.Crafter, Item.MaterialType.None, Item.StateType.Solid));
-            possibleItems.Add(new Item(2, "Miner", Item.ItemType.Miner, Item.MaterialType.None, Item.StateType.Solid));
-            possibleItems.Add(new Item(3, "Polisher", Item.ItemType.Polisher, Item.MaterialType.None, Item.StateType.Solid));
-            possibleItems.Add(new Item(4, "Smelter", Item.ItemType.Smelter, Item.MaterialType.None, Item.StateType.Solid));
+            possibleItems = new List<Item>
+            {
+                new Item(0, "IronOre", Item.ItemType.Ore, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(0, "IronBar", Item.ItemType.Bar, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(0, "IronPlate", Item.ItemType.Plate, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(1, "Crafter", Item.ItemType.Crafter, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(2, "Miner", Item.ItemType.Miner, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(3, "Polisher", Item.ItemType.Polisher, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(4, "Smelter", Item.ItemType.Smelter, Item.MaterialType.None, Item.StateType.Solid)
+            };
         }
     }
 }
