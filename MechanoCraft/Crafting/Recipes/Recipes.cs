ab6e079d3401142bd28519026674ab7431b8c0a6
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-01-23 14:43:08 +0100

    feat: added recipes

---
+++ MechanoCraft/Crafting/Recipes/Recipes.cs
@@ -0,0 +1,41 @@
+﻿using MechanoCraft.Inventory;
+using MechanoCraft.Inventory.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanoCraft.Crafting.Recipes
+{
+    public class Recipes
+    {
+        static Dictionary<List<Item>, List<Item>> possibleRecipes;
+
+        public static void CreateRecipes()
+        {
+            List<Item> inputs = new List<Item>();
+            List<Item> outputs = new List<Item>();
+
+
+            possibleRecipes = new Dictionary<List<Item>, List<Item>> ();
+
+            inputs.Add(ItemCreator.possibleItems[1]);
+            outputs.Add(ItemCreator.possibleItems[2]);
+            possibleRecipes.Add(inputs, outputs);
+
+            inputs.Clear();
+            outputs.Clear();
+
+            inputs.Add(ItemCreator.possibleItems[2]);
+            outputs.Add(ItemCreator.possibleItems[3]);
+            possibleRecipes.Add(inputs, outputs);
+
+            inputs.Clear();
+            outputs.Clear();
+
+
+        }
+
+    }
+}
--- MechanoCraft/Inventory/ItemCreator.cs
+++ MechanoCraft/Inventory/ItemCreator.cs
@@ -14,12 +14,13 @@ namespace MechanoCraft.Inventory
         {
             possibleItems = new List<Item>
             {
-                new Item(0, "IronOre", Item.ItemType.Ore, Item.MaterialType.Iron, Item.StateType.Solid),
-                new Item(0, "IronBar", Item.ItemType.Bar, Item.MaterialType.Iron, Item.StateType.Solid),
-                new Item(0, "IronPlate", Item.ItemType.Plate, Item.MaterialType.Iron, Item.StateType.Solid),
-                new Item(1, "Crafter", Item.ItemType.Crafter, Item.MaterialType.None, Item.StateType.Solid),
-                new Item(2, "Miner", Item.ItemType.Miner, Item.MaterialType.None, Item.StateType.Solid),
-                new Item(3, "Polisher", Item.ItemType.Polisher, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(0, "None", Item.ItemType.None, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(1, "IronOre", Item.ItemType.Ore, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(2, "IronBar", Item.ItemType.Bar, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(3, "IronPlate", Item.ItemType.Plate, Item.MaterialType.Iron, Item.StateType.Solid),
+                new Item(4, "Crafter", Item.ItemType.Crafter, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(5, "Miner", Item.ItemType.Miner, Item.MaterialType.None, Item.StateType.Solid),
+                new Item(6, "Polisher", Item.ItemType.Polisher, Item.MaterialType.None, Item.StateType.Solid),
                 new Item(4, "Smelter", Item.ItemType.Smelter, Item.MaterialType.None, Item.StateType.Solid)
             };
         }
