ac587e7b6678cf9fc1ce9ea6948e7b2a70b0b20a
Vivien Piccin <Vivien.Piccin@eduvaud.ch>
2024-02-06 15:45:41 +0100

    Remove render import in MechanoCraft

--- MechanoCraft/MechanoCraft.cs
+++ MechanoCraft/MechanoCraft.cs
@@ -1,6 +1,5 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MechanoCraft.Render;
 using MechanoCraft.Crafting.Recipes;
 using MechanoCraft.Inventory;
 using MonoGame.Extended.Tiled;
